100f512e834966d215ae8f4bc4e629950accf1c5
Irina Stoica <Irina.STOICA@student.utcluj.ro>
2020-01-06 19:19:34 +0200

    first commit

---
+++ 3.13/cmake-build-debug/CMakeFiles/3_13.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "3_13.exe"
+  "3_13.exe.manifest"
+  "3_13.pdb"
+  "CMakeFiles/3_13.dir/main.c.obj"
+  "lib3_13.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/3_13.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
